87b6c9e53bdcbcf15202ee93e0eb108b98c6ab02
bharath21sep <bharath4048@gmail.com>
2021-10-27 10:05:06 -0600

    committing

---
+++ src/sample/EclipseSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+
+public class EclipseSample {
+
+	public static void main(String[] args) {
+		int temp=0,number=537;
+		temp = number % 10;
+		System.out.println(temp);
+		//int total = total + temp * temp * temp;
+		number/=10;
+		System.out.println(number);
+ }
+
+}
