fc2418206ddf4b1db7ee59a26e5ad7a8a9b1db7e
bharath21sep <bharath4048@gmail.com>
2021-11-23 00:53:09 -0700

    suck

--- src/sample/EclipseSample.java
+++ src/sample/EclipseSample.java
@@ -11,10 +11,12 @@ public class EclipseSample {
 		//int total = total + temp * temp * temp;
 		number/=10;
 		System.out.println(number);
- }
+ 
 
 		Scanner input = new Scanner(System.in);
 
 		 int count = input.nextInt();
 		 int n1=0,n2=1,n3,i;
+		 System.out.println("Additional Of Two Numbers is "+ n1);
+}
 }
